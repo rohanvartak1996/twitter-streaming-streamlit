8e336a964fe54b9db41887be19b4274d38e55486
Rohan Vartak <rohanvartak1996@gmail.com>
2019-10-24 15:18:06 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"rohanvartak1996@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
